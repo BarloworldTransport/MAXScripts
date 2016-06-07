a685a302bf51a5386989c8ef87660e0ac13186b1
Feighen Oosterbroek <feighen@gmail.com>
2016-06-07 13:07:03 +0200

    added reports/stop_order_report.sh -> Add a report to Max that allows us to search the added stop orders

---
+++ reports/stop_order_report.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+clia DataView createItemList objectRegistry="udo_StopOrder" name="Stop Order Report" internalCreator="Report" primaryOwner="Admin" primaryOwnerCrud="Create, Read, Update, Delete" groupOwner="Reports" groupOwnerCrud="Read"
+clia DataView setShowOptions objectRegistry="udo_StopOrder" type="ItemListDataView" name="Stop Order Report" detailAction="Open" showActions=0 showExport=1 showFooterBar=1
+clia DataView setIcon objectRegistry="udo_StopOrder" type="ItemListDataView" name="Stop Order Report" icon="icon-reports.gif"
+clia DataView setPageSize objectRegistry="udo_StopOrder" type="ItemListDataView" name="Stop Order Report" pageSize="500"
+clia DataView setNoRecordsMessage objectRegistry="udo_StopOrder" type="ItemListDataView" name="Stop Order Report" message="Unfortunately no records were returned."
+clia DataView addParameterDateTime objectRegistry=udo_StopOrder type=ItemListDataView name="Stop Order Report" parameterHandle="beginDate" parameterName="Start Date" mandatory="1"
+clia DataView addParameterDateTime objectRegistry=udo_StopOrder type=ItemListDataView name="Stop Order Report" parameterHandle="stopDate" parameterName="Stop Date" mandatory="1"
+clia DataView addParameterSelect objectRegistry=udo_StopOrder type=ItemListDataView name="Stop Order Report" parameterHandle="driver" parameterName="Driver" mandatory="0" objectRegistryToSelect="udo_Driver" filter='active = "true"'
+clia DataView setFilter objectRegistry="udo_StopOrder" type="ItemListDataView" name="Stop Order Report" filter='driver_id = @Parameter:driver AND time_created>=@Parameter:beginDate AND time_created<@Parameter:stopDate AND _status = null'
+clia DataView setOrderBy objectRegistry="udo_StopOrder" type="ItemListDataView" name="Stop Order Report" orderByField=driver_id orderByDirection=ASC
+clia DataView setOrderBy objectRegistry="udo_StopOrder" type="ItemListDataView" name="Stop Order Report" orderByField=id orderByDirection=ASC
+clia DataView addField objectRegistry=udo_StopOrder type=ItemListDataView name="Stop Order Report" label="Driver" source="driver_id"
+clia DataView addField objectRegistry=udo_StopOrder type=ItemListDataView name="Stop Order Report" label="Reason" source="reason"
+clia DataView addField objectRegistry=udo_StopOrder type=ItemListDataView name="Stop Order Report" label="Time Created" source="time_created"
+clia DataView addField objectRegistry=udo_StopOrder type=ItemListDataView name="Stop Order Report" label="Created By" source="created_by"
+clia Report create name="Stop Order Report" objectRegistry=udo_StopOrder dataView="Stop Order Report" dataViewType=ItemListDataView primaryOwner="Justin Ward" primaryOwnerCrud="Create, Read, Update, Delete" groupOwner="Manline" groupOwnerCrud="Read"
