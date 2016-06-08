55861bf0de51f6ebc0befe3a2404d35f5d959363
Feighen Oosterbroek <feighen@gmail.com>
2016-06-08 10:30:19 +0200

    Added reports/trips_.....sh. These are changes for 1) grouping so that it only displays a single row per cargo, and 2) so that the filter works the same way as the income report (loadingStarted v LoadingArrival)

---
+++ reports/trips_per_customer_updates_group_and_filter.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+clia DataView setFilter objectRegistry="udo_Cargo" type="ItemListDataView" name="Trips per Customer" filter='(udo_TripLegCargo.cargo_id->tripLeg_id->workshopTrip = "0" OR udo_TripLegCargo.cargo_id->tripLeg_id->workshopTrip = null) AND customer_id = @Parameter:customer AND businessUnit_id IN @Parameter:businessUnit_id AND ((udo_TripLegCargo.cargo_id->tripLeg_id->loadingStarted != null AND udo_TripLegCargo.cargo_id->tripLeg_id->loadingStarted >= @Parameter:startDate AND udo_TripLegCargo.cargo_id->tripLeg_id->loadingStarted <= @Parameter:stopDate) OR (udo_TripLegCargo.cargo_id->tripLeg_id->loadingStarted = null AND udo_TripLegCargo.cargo_id->tripLeg_id->loadingArrivalETA >= @Parameter:startDate AND udo_TripLegCargo.cargo_id->tripLeg_id->loadingArrivalETA <= @Parameter:stopDate))'
+clia Report setGroupBy name='Trips per Customer' newGroupBy=id
