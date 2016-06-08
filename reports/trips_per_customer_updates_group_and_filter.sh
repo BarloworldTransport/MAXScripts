36b3dd5ae151b7931b5391d1c02dddc0db93d8be
Feighen Oosterbroek <feighen@gmail.com>
2016-06-08 10:43:49 +0200

    Added line comments to detail what I am doing

--- reports/trips_per_customer_updates_group_and_filter.sh
+++ reports/trips_per_customer_updates_group_and_filter.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+# Change the filter so that it works off of loading Started if available
 clia DataView setFilter objectRegistry="udo_Cargo" type="ItemListDataView" name="Trips per Customer" filter='(udo_TripLegCargo.cargo_id->tripLeg_id->workshopTrip = "0" OR udo_TripLegCargo.cargo_id->tripLeg_id->workshopTrip = null) AND customer_id = @Parameter:customer AND businessUnit_id IN @Parameter:businessUnit_id AND ((udo_TripLegCargo.cargo_id->tripLeg_id->loadingStarted != null AND udo_TripLegCargo.cargo_id->tripLeg_id->loadingStarted >= @Parameter:startDate AND udo_TripLegCargo.cargo_id->tripLeg_id->loadingStarted <= @Parameter:stopDate) OR (udo_TripLegCargo.cargo_id->tripLeg_id->loadingStarted = null AND udo_TripLegCargo.cargo_id->tripLeg_id->loadingArrivalETA >= @Parameter:startDate AND udo_TripLegCargo.cargo_id->tripLeg_id->loadingArrivalETA <= @Parameter:stopDate))'
+# Add the grouping so that it groups on the cargoID
 clia Report setGroupBy name='Trips per Customer' newGroupBy=id
