92a2140b2b56f117ee19c883fcdc71a14b6162d9
Feighen Oosterbroek <feighen@gmail.com>
2016-08-02 10:19:34 +0200

    Added column for pre-loading driver

--- reports/Triplist_by_fleet_added_columns.sh
+++ reports/Triplist_by_fleet_added_columns.sh
@@ -2,3 +2,6 @@
 
 clia DataView addField objectRegistry=udo_TripLeg type=ItemListDataView name="Triplist by Fleet" label="Primary Fleet" source="truck_id->primaryFleet_id"
 clia DataView addField objectRegistry=udo_TripLeg type=ItemListDataView name="Triplist by Fleet" label="Tripleg Income" source="tripLegIncome"
+
+# 2016-08-02
+clia DataView addField objectRegistry=udo_TripLeg type=ItemListDataView name="Triplist by Fleet" label="Pre-Loading Driver" source="preLoadingDriver_id"
