36e1eaeca941e0d34bfbc136f877bed7ddabc427
Feighen Oosterbroek <feighen@gmail.com>
2016-08-19 12:02:28 +0200

    new report fields. Product* into trips by truck, and Cust Syspro code in

--- reports/trips_by_truck.sh
+++ reports/trips_by_truck.sh
@@ -2,3 +2,8 @@
 
 clia DataView addField objectRegistry=udo_TripLegCargo type=ItemListDataView name="Trips by Truck" label="Loading Region" source="tripLeg_id->locationFromPoint_id->parent_id->parent_id->name" showInRelatedWidget=0
 clia DataView addField objectRegistry=udo_TripLegCargo type=ItemListDataView name="Trips by Truck" label="Offloading Region" source="tripLeg_id->locationToPoint_id->parent_id->parent_id->name" showInRelatedWidget=0
+
+# 2016-08-19
+clia DataView addField objectRegistry=udo_TripLegCargo type=ItemListDataView name="Trips by Truck" label="Product Category" source="cargo_id->productCategory_id->name" showInRelatedWidget=0
+clia DataView addField objectRegistry=udo_TripLegCargo type=ItemListDataView name="Trips by Truck" label="Product Type" source="cargo_id->productType_id->name" showInRelatedWidget=0
+clia DataView addField objectRegistry=udo_TripLegCargo type=ItemListDataView name="Trips by Truck" label="Product Detail" source="cargo_id->productDetail_id->name" showInRelatedWidget=0
---
+++ reports/trips_to_invoice.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# 2016-08-19 ticket number: 10635 
+clia DataView addField objectRegistry=udo_Cargo type=ItemListDataView name="Trips to Invoice" label="Customer Syspro Code" source="customer_id->sYSProCode" showInRelatedWidget=0
